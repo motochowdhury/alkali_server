f600f1a1512e19387351d2836078813285effc84
motochowdhury <motochowdhury@gmail.com>
2023-06-18 20:08:09 +0600

    morgan setup done

--- src/server.js
+++ src/server.js
@@ -2,8 +2,16 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const morgan = require("morgan");
 
+// Middleware
+app.use(morgan("dev"));
 
+// Get request
+// Root derectory
+app.get("/", (req,res) => {
+res.send("Welcome to server")
+})
 
 // App listener
 app.listen(port, ()=> {
