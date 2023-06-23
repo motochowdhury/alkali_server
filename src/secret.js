c363b875b3d147b846a05909fa33d5f44e0ff571
motochowdhury <motochowdhury@gmail.com>
2023-06-23 19:56:05 +0600

    secret file has added mongoDB url

--- src/secret.js
+++ src/secret.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 const serverPort = process.env.SERVER_PORT || 5001
+const mongoDbUrl = process.env.MONGODB_ATLAS_URL || "mongodb://localhost:27017/alkaliDB"
 
-module.exports = serverPort
+module.exports = {serverPort, mongoDbUrl}
