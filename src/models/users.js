2b67b35d2acfab8bf8f44438cc75405b945cdce0
motochowdhury <motochowdhury@gmail.com>
2023-06-25 21:58:47 +0600

    email validation is done

--- src/models/users.js
+++ src/models/users.js
@@ -7,5 +7,18 @@ const userSchema = new Schema({
         trim: true,
         min: [3, 'Minimum 3 charecter required'],
         max: [30, 'Maximum 30 charecter is acceptable']
+    },
+    email: {
+        type: String,
+        require: [true, 'Email is required'],
+        trim: true,
+        toString: true,
+        validate: {
+            validator: (v)=>{
+                return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v)
+                message: "This is not a valid email"
+            }
+        },
+        unique: true
     }
 })
