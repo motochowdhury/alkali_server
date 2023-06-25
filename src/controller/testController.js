dc22e968a0fe4b6ac1e6c905949b2986f4f7e678
motochowdhury <motochowdhury@gmail.com>
2023-06-25 21:46:07 +0600

    user model removed

--- src/controller/testController.js
+++ src/controller/testController.js
@@ -1,10 +1,7 @@
-const user = require("../models/users");
-
 const test = (req,res) => {
     console.log(req.body.id)
     res.status(200).send({
     message: "Hey Welcome to the server",
-    user
     })
 }
 
