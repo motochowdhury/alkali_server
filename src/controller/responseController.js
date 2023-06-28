6404c249dff6bae74532e53f25eadee92acf570b
motochowdhury <motochowdhury@gmail.com>
2023-06-28 12:28:22 +0600

    successResponse controller done

--- src/controller/responseController.js
+++ src/controller/responseController.js
@@ -1,7 +1,17 @@
+// For handling error
 const errorResponse = (res, {statusCode = 500, message = "Internal server error"}) => {
     return res.status(statusCode).json({
         sucess: false,
         message,
+    })
+}
+
+// For handling success response
+const successResponse = (res, {statusCode = 200, message = "Success done",palyload}) => {
+    return res.status(statusCode).json({
+        sucess: true,
+        message,
+        palyload
     })
 }
 
