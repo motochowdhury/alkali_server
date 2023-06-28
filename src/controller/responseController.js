2ec8ee8f55c0e2365085eb2108e59e3c53e36ac9
motochowdhury <motochowdhury@gmail.com>
2023-06-28 12:19:56 +0600

    errorResponse controller done

---
+++ src/controller/responseController.js
@@ -0,0 +1,6 @@
+const errorResponse = (res, {statusCode = 500, message = "Internal server error"}) => {
+    return res.status(statusCode).json({
+        sucess: false,
+        message,
+    })
+}
