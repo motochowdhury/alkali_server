3193a1c3b4f2b9c3ff2f2270d81a5a0bda364989
motochowdhury <motochowdhury@gmail.com>
2023-07-05 19:50:19 +0600

    userRoute commented sucessfully

--- src/routers/userRoute.js
+++ src/routers/userRoute.js
@@ -3,9 +3,14 @@ const {getUsers, getUser, deleteUser} = require('../controller/UserController')
 const expressRouter = express.Router();
 
 // Get request
-// Root derectory
+
+// get all users
 expressRouter.get("/users", getUsers);
+
+// get single user
 expressRouter.get("/users/:id", getUser);
+
+// delete single user
 expressRouter.delete("/users/:id", deleteUser);
 
 module.exports = expressRouter;
