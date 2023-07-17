7097dcce54810bd22a24cb6aa49dfbabad832010
motochowdhury <motochowdhury@gmail.com>
2023-07-17 09:41:39 +0600

    refectoring bcrypt to bcryptjs done

--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,5 @@
 const {Schema, model} = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { profileImage } = require("../secret");
 
 const userSchema = new Schema({
